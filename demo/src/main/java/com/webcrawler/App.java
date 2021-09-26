4cc34184289433e464f5b4ab61dd192f9defe1cc
nguyentlan04@gmail.com <nguyentlan04@gmail.com>
2021-09-25 19:23:25 -0700

    added basic web crawler (step 1)

--- demo/src/main/java/com/webcrawler/App.java
+++ demo/src/main/java/com/webcrawler/App.java
@@ -1,13 +1,47 @@
 package com.webcrawler;
 
-/**
- * Hello world!
- *
- */
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+import java.io.IOException;
+import java.util.HashSet;
+
+
 public class App 
 {
+    private HashSet<String> links; 
+
+    public App()
+    {
+        links = new HashSet<String>();
+    }
+
+    public void getPageLinks(String URL){
+
+        if(!links.contains(URL)){
+            try{
+                
+                if(links.add(URL)){
+                    System.out.println(URL);
+                }
+
+                Document document = Jsoup.connect(URL).get(); 
+                
+                Elements linksOnPage = document.select("a[hfef]"); 
+
+                for(Element page: linksOnPage){
+                    getPageLinks(page.attr("abs:href"));
+                }
+
+            } catch(IOException e){
+                System.err.println("For '" + URL + "':" + e.getMessage());
+            }
+        }
+    }
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        new App().getPageLinks("http://www.mkyong.com/");
     }
 }
