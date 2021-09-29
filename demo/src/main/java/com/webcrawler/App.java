43f3f6e6b85f7cbdc3017d8aff10aebc4e0f04f2
nguyentlan04@gmail.com <nguyentlan04@gmail.com>
2021-09-28 17:15:18 -0700

    Built basic framework

--- demo/src/main/java/com/webcrawler/App.java
+++ demo/src/main/java/com/webcrawler/App.java
@@ -7,41 +7,56 @@ import org.jsoup.select.Elements;
 
 import java.io.IOException;
 import java.util.HashSet;
+import java.io.FileWriter;
+
 
 
 public class App 
 {
+    
+    private static final int MAX_DEPTH = 2;
     private HashSet<String> links; 
 
     public App()
     {
-        links = new HashSet<String>();
+        links = new HashSet<>();
     }
 
-    public void getPageLinks(String URL){
+    public void getPageLinks(String URL, int depth){
 
-        if(!links.contains(URL)){
+        if(!links.contains(URL) && (depth < MAX_DEPTH)){
+            System.out.println(">> Depth: " + depth + " [" + URL + "]");
+            
             try{
-                
-                if(links.add(URL)){
-                    System.out.println(URL);
-                }
 
                 Document document = Jsoup.connect(URL).get(); 
-                
-                Elements linksOnPage = document.select("a[hfef]"); 
+                Elements linksOnPage = document.select("a[href]"); 
+
+                depth++;
 
                 for(Element page: linksOnPage){
-                    getPageLinks(page.attr("abs:href"));
+                    getPageLinks(page.attr("abs:href"), depth);
                 }
 
             } catch(IOException e){
                 System.err.println("For '" + URL + "':" + e.getMessage());
             }
+
         }
     }
+
+    /*public void writeToFile(String filename){
+        Filewriter writer;
+        try {
+            writer = new FileWriter(filename); 
+            //articles.forEach\
+            String temp = " - Title: " + a.get(0) + " (link: " + a.get(1) + ")\n"; 
+        }
+        catch 
+
+    }*/
     public static void main( String[] args )
     {
-        new App().getPageLinks("http://www.mkyong.com/");
+        new App().getPageLinks("https://www.mkyong.com/", 0);
     }
 }
