2a4644c04e43b6955fc079f94a1cbff82e9bfd0b
a-zhongg <61555438+a-zhongg@users.noreply.github.com>
2021-10-02 22:26:07 -0700

    Update getWords.py

--- Analysis/getWords.py
+++ Analysis/getWords.py
@@ -1,37 +1,42 @@
-import re 
-from operator import itemgetter
-import numpy as np 
-import matplotlib.pyplot as plt
-from scipy import special 
+import xlsxwriter
+from stats import text_in_dir_stats
 
-frequency = {}
-open_file = open('repository/Android Developers.html', 'r')
+# Root directory with all texts
+root_dir='repository'
 
-file_to_string = open_file.read()
-words = re.findall(r'(\b[A-Za-z][a-z]{2,9}\b)', file_to_string)
+# Check Zipf's law
+print('Working on Zipf\'s law...')
+dictionary = text_in_dir_stats(root_dir)
+sorted_dict = sorted(dictionary.items(), key=lambda x: (x[1],x[0]), reverse=True)
+workbook = xlsxwriter.Workbook('Zipf.xlsx')
+worksheet = workbook.add_worksheet()
+worksheet.write(0, 0, 'Rank')
+worksheet.write(0, 1, 'Count')
+worksheet.write(0, 2, 'Word')
+row = 1
+total_words = 0
+for [key, value] in sorted_dict:
+	worksheet.write(row, 0, row)
+	worksheet.write(row, 1, value)
+	worksheet.write(row, 2, key)
+	total_words += value
+	row += 1
+workbook.close()
+print('Done! Total words count: ' + str(total_words) + '\n')
 
-for word in words: 
-        count = frequency.get(word,0)
-        frequency[word]= count +1
-
-#for key, value in reversed(sorted(frequency.items(), key = itemgetter(1))): 
-#   print(key,value) 
-
-n = 1000
-frequency = {key:value for key,value in frequency.items()}
-
-s = list(frequency.items())
-# s = frequency.values()
-s = np.array(s)
-print(s)
-
-a = 2. 
-
-for x in s: 
-        count,bins, ignored = plt.hist(s[s[x*2]<50], 50, normed = True)
-
-#count,bins, ignored = plt.hist(s[s<50], 50, normed = True)
-x = np.arrange(1.,50.)
-y = x**(-a) / special.zetac(a)
-plt.plot(x, y/max(y), linewidth=2, color='r')
-plt.show()
+# Check Heaps' law
+print('Working on Heaps\' law...')
+files = 5 # of files being read
+workbook = xlsxwriter.Workbook('Heaps.xlsx')
+worksheet = workbook.add_worksheet()
+worksheet.write(0, 0, 'Texts analyzed')
+worksheet.write(0, 1, 'Unique Word Count')
+worksheet.write(0, 2, 'Total words in texts')
+for max_files in range (1, files):
+	dictionary = text_in_dir_stats(root_dir, max_files)
+	worksheet.write(max_files, 0, max_files)
+	worksheet.write(max_files, 1, len(dictionary))
+	worksheet.write(max_files, 2, sum(dictionary.values()))
+	print('Progress: ' + str(round(100 * max_files / files)) + '%', end='\r')
+workbook.close()
+print('\nDone!')
