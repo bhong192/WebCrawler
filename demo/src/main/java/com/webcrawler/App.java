4f06f6070232c1fc1a9273d18624aa08dc247ba8
nguyentlan04@gmail.com <nguyentlan04@gmail.com>
2021-10-05 17:23:08 -0700

    Added language detection pog :))

--- demo/src/main/java/com/webcrawler/App.java
+++ demo/src/main/java/com/webcrawler/App.java
@@ -26,30 +26,24 @@ import java.util.regex.Pattern;
 import javax.lang.model.util.ElementScanner6;
 
 import java.util.regex.Matcher; 
+import com.detectlanguage.DetectLanguage;
 
 
 
 public class App 
 {
     
-    private static final int MAX_DEPTH = 2;
-    //public static String titleOne = "URL";
-    //public static int titleTwo = 25; 
+    private static final int MAX_DEPTH = 1;
     private HashMap<String, Integer> links; 
     private String[] removedTags = {"audio", "button", "img", "input", "nav", "video", "script", "style", "a", "link", "footer", "object", "figure", "track", "noscript", "form" };
 
     public App()
     {
         links = new HashMap<String, Integer>();
-
-        //int titleTwo = 25;
-        //int titleTwo = Integer.parseInt(title);
     
     }
 
     public void getLinks(String URL, int depth) throws IOException{
-
-        //links.put(titleOne, titleTwo);
 
         if(!links.containsKey(URL) && (depth < MAX_DEPTH)){
             System.out.println(">> Depth: " + depth + " [" + URL + "]");
@@ -60,11 +54,7 @@ public class App
                 Elements linksOnPage = document.select("a[href]"); 
 
 
-
                 int outlinks = Integer.valueOf(linksOnPage.size());
-                String pathToCsv = "./report.csv";
-                File csvFile = new File(pathToCsv);
-                //FileWriter csvWriter = new FileWriter(csvFile, true);
 
                 links.put(URL, outlinks);
                 //System.out.println(links);
@@ -87,10 +77,10 @@ public class App
                 }
                 
                 String lang = getLang(document.body().text().substring(0,Math.min(75, document.body().text().length())));
-                if (!lang.equals("English")){
-                    System.out.println("Not English");
-                    System.out.println(lang);
+                if (!lang.equals("en")){
+                    System.out.println("Not English, it's " + lang);
                 }
+
                 System.out.println(lang);
 
                 String path = "./repository/" + lang;
@@ -121,8 +111,7 @@ public class App
     }
     public String getLang(String sampleText){ 
 
-        String languageLayerEndPoint = "http://api.languagelayer.com/detect?access_key=%s&query=%s";
-        String APIKey= "1ca250f0a2bbcb6ed2da2a9e17e0b720";
+        DetectLanguage.apiKey = "0a6eac1e86ea2d44613723b79536da4d";
 
         String newSampleText = "";
 
@@ -133,40 +122,17 @@ public class App
         }
 
         // need to format a string to use as API request
-        String request = String.format(languageLayerEndPoint, APIKey, newSampleText); 
         String language = "";
-        
-        try{
-            URL url = new URL(request);
-            HttpURLConnection connection = (HttpURLConnection) url.openConnection(); 
-            InputStream is = connection.getInputStream(); 
-
-            String text = null; 
-            try(Scanner scanner = new Scanner(is)){
-                text = scanner.useDelimiter("\\A").next(); 
-            }
-            is.close(); 
-            connection.disconnect();
-
-            // we have a response
-            System.out.println(text); 
-            String regex = "\"language_name\":\\s*\"\\w+\""; 
-            Pattern pattern = Pattern.compile(regex); 
-            Matcher matcher = pattern.matcher(text); 
-
-            while(matcher.find()){
-                System.out.println("Start index: " + matcher.start()); 
-                System.out.println("End index: " + matcher.end());
-                System.out.println("Found: " + matcher.group());
-                String lang1 = text.substring(matcher.start(), matcher.end()); 
-                language = (lang1.replaceAll("(\"|\\s)","").split(":")[1]);
-            }
-        } catch (Exception e){
+        try{            
+            language = DetectLanguage.simpleDetect(newSampleText); 
+        }catch (Exception e){
             e.printStackTrace();
             System.out.println(e.getMessage()); 
             language = "Unknown"; 
         }
+
         return language; 
+        
     }
 
     public static void main( String[] args ) throws IOException
@@ -185,13 +151,10 @@ public class App
         for(HashMap.Entry<String, Integer> entry: finishedlinks.entrySet()){
             
             csvWriter.append(entry.getKey() + ", " + entry.getValue());
-            csvWriter.append("\n");       
-                 
+            csvWriter.append("\n");                       
         }
 
         csvWriter.close();
-
-
 
     }
 }
